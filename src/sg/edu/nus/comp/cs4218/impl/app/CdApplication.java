427e451be7613a09d4d7360457d2d03359bd3020
AnHaNguyen <edge.skywalker@gmail.com>
2017-02-07 19:19:33 +0800

    fix bug cd and add all exceptions file

--- src/sg/edu/nus/comp/cs4218/impl/app/CdApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/CdApplication.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -9,6 +11,7 @@ import java.nio.file.Paths;
 import sg.edu.nus.comp.cs4218.Application;
 import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.CdException;
 
 public class CdApplication implements Application{
 
@@ -16,15 +19,24 @@ public class CdApplication implements Application{
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
 		// TODO Auto-generated method stub
 		if (args.length != 1) {
-			throw new IllegalArgumentException("Application requires 1 argument");
+			throw new CdException("Application requires 1 argument");
 		}
-		Path currentDir = Paths.get(Environment.currentDirectory);
-		Path newDir = currentDir.resolve(args[0]);
-		if (Files.isDirectory(newDir)) {
-			System.setProperty("user.dir", newDir.toString());
-			Environment.currentDirectory = newDir.toString();	
+		String curDir = Environment.currentDirectory;
+		String newDir = curDir + File.separator + args[0];
+
+		File pathToDir = new File(newDir);
+		
+		if (pathToDir.exists()) {
+			try {
+				String finalPath = pathToDir.getCanonicalPath();
+				System.setProperty("user.dir", finalPath);
+				Environment.currentDirectory = finalPath;	
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new CdException("Cannot get canonical Path");
+			}
 		} else {
-			throw new IllegalArgumentException("This is not a directory");
+			throw new CdException("This is not a directory");
 		}
 	}
 }
