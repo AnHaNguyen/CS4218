86d66d8de67ff3846393a44c21c53ff04734919e
Thien Tran <thientran1707@gmail.com>
2017-02-11 23:38:39 +0800

    refactor string and number to constants

--- src/sg/edu/nus/comp/cs4218/Constants.java
+++ src/sg/edu/nus/comp/cs4218/Constants.java
@@ -9,13 +9,16 @@ public class Constants {
         public static final int CALENDAR_COL_SIZE = 7;
         public static final int YEAR_ROW_SIZE = 4;
         public static final int YEAR_COL_SIZE = 3;
+        public static final String SPACE_BETWEEN_MONTH = "        ";
         public static final String[] MONTH_TO_TEXT = {
         	"Jannuary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
         };
-        public static final String SPACE_BETWEEN_MONTH = "        ";
     }
     
     public class CalMessage {
-    	
+    	public static final String INVALID_ARGS = "Agrs is null";
+    	public static final String INVALID_NUMBER_ARGUMENTS = "Invalid number of arguments";
+    	public static final String INVALID_YEAR = "Invalid year";
+    	public static final String INVALID_MONTH = "Invalid month";
     }
 }
--- src/sg/edu/nus/comp/cs4218/impl/app/CalApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/CalApplication.java
@@ -13,52 +13,60 @@ import sg.edu.nus.comp.cs4218.Constants;
 import sg.edu.nus.comp.cs4218.Utility;
 
 public class CalApplication implements Cal {
-
+	static final String MONDAY_FIRST_FLAG = "-m";
+	static final String ONE_SPACE = " ";
+	static final String TWO_SPACE = "  ";
+	static final String THREE_SPACE = "   ";
+	static final int MONTHS_PER_YEAR = 12;
+	
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws CalException {		
 		boolean isMondayFirst = false;
 		List<String> paramsList = new ArrayList<String>(Arrays.asList(args));
 		
 		if (args == null) {
-			throw new CalException("args is null");
+			throw new CalException(Constants.CalMessage.INVALID_ARGS);
 		}
 	
 		if (args.length > 3) {
-			throw new CalException("Invalid number of arguments");
+			throw new CalException(Constants.CalMessage.INVALID_NUMBER_ARGUMENTS);
 		}
 		
-		if (args.length > 0 && args[0].equals("-m")) {
+		if (args.length > 0 && args[0].equals(MONDAY_FIRST_FLAG)) {
 			isMondayFirst = true;
 			paramsList.remove(0);
 		}
 		
 		if (paramsList.size() == 0) {
+			// print current month and year
 			Calendar calendar = Calendar.getInstance();
 			int month = calendar.get(Calendar.MONTH);
 			int year = calendar.get(Calendar.YEAR);
 			printCalendarForMonthYear(month, year, isMondayFirst);
 		} else if (paramsList.size() == 1) {
+			// print all month of year
 			try {
 				int year = Integer.parseInt(paramsList.get(0));
 				if (year < 0) {
-					throw new CalException("Invalid year input");
+					throw new CalException(Constants.CalMessage.INVALID_YEAR);
 				}
 				
 				printCalendarForYear(year, isMondayFirst);
 			} catch(Exception e) {
-				throw new CalException("Invalid year input");
+				throw new CalException(e.getMessage());
 			}
 		} else if (paramsList.size() == 2) {
+			// print month and year from user
 			try {
 				int month = Integer.parseInt(paramsList.get(0));
 				int year = Integer.parseInt(paramsList.get(1));
 				
 				if (year < 0) {
-					throw new CalException("Invalid year");
+					throw new CalException(Constants.CalMessage.INVALID_YEAR);
 				}
 				
 				if (month <= 0 || month > 12) {
-					throw new CalException("Invalid month");
+					throw new CalException(Constants.CalMessage.INVALID_MONTH);
 				}
 				
 				printCalendarForMonthYear(month - 1, year, isMondayFirst); // month is from 0 to 11
@@ -70,7 +78,6 @@ public class CalApplication implements Cal {
 
 	public void printCalendarForMonthYear(int month, int year, boolean mondayFirst) {
 		int[][] monthArr = Utility.getCalendarArrayForMonth(month, year, mondayFirst);
-		// print month and year
 		Utility.printCalTitle(month, year);
 		System.out.println();
 		
@@ -80,11 +87,11 @@ public class CalApplication implements Cal {
 		for (int i = 0; i < Constants.Common.CALENDAR_ROW_SIZE; i++) {
 			for (int j = 0; j < Constants.Common.CALENDAR_COL_SIZE; j++) {
 				if (monthArr[i][j] == -1) {
-					System.out.print("   ");
+					System.out.print(THREE_SPACE);
 				} else if (monthArr[i][j] < 10) {
-					System.out.print(monthArr[i][j] + "  ");
+					System.out.print(monthArr[i][j] + TWO_SPACE);
 				} else {
-					System.out.print(monthArr[i][j] + " ");
+					System.out.print(monthArr[i][j] + ONE_SPACE);
 				}
 			}
 			System.out.println();
@@ -93,13 +100,13 @@ public class CalApplication implements Cal {
 	
 	public void printCalendarForYear(int year, boolean isMondayFirst) {
 		List<int[][]> yearArray = new ArrayList<int[][]>();
-		for (int month = 0; month < 12; month++) {
+		for (int month = 0; month < MONTHS_PER_YEAR; month++) {
 			int[][] monthArr = Utility.getCalendarArrayForMonth(month, year, isMondayFirst);
 			yearArray.add(monthArr);
 		}
 
-		for (int month = 0; month < 12; month += 3) {
-			printMonthCalendarInRow(year, isMondayFirst, month, month + 2, yearArray);
+		for (int month = 0; month < MONTHS_PER_YEAR; month += Constants.Common.YEAR_COL_SIZE) {
+			printMonthCalendarInRow(year, isMondayFirst, month, month + Constants.Common.YEAR_COL_SIZE - 1, yearArray);
 			System.out.println();
 		}
 		
@@ -125,11 +132,11 @@ public class CalApplication implements Cal {
 				int[][] monthArr = yearArray.get(month);
 				for (int j = 0; j < Constants.Common.CALENDAR_COL_SIZE; j++) {
 					if (monthArr[i][j] == -1) {
-						System.out.print("   ");
+						System.out.print(THREE_SPACE);
 					} else if (monthArr[i][j] < 10) {
-						System.out.print(monthArr[i][j] + "  ");
+						System.out.print(monthArr[i][j] + TWO_SPACE);
 					} else {
-						System.out.print(monthArr[i][j] + " ");
+						System.out.print(monthArr[i][j] + ONE_SPACE);
 					}
 				}
 				Utility.printMonthSpace();
