865508dd91fbbfb82671f50a1b2e7df77d644495
Thien Tran <thientran1707@gmail.com>
2017-02-15 12:58:55 +0800

    implement all calapplication interface methods

--- src/sg/edu/nus/comp/cs4218/Constants.java
+++ src/sg/edu/nus/comp/cs4218/Constants.java
@@ -5,14 +5,7 @@ package sg.edu.nus.comp.cs4218;
  */
 public class Constants {
     public static class Common {
-        public static final int CALENDAR_ROW_SIZE = 6;
-        public static final int CALENDAR_COL_SIZE = 7;
-        public static final int YEAR_ROW_SIZE = 4;
-        public static final int YEAR_COL_SIZE = 3;
-        public static final String SPACE_BETWEEN_MONTH = "        ";
-        public static final String[] MONTH_TO_TEXT = {
-        	"Jannuary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-        };
+
     }
     
     public class CalMessage {
--- src/sg/edu/nus/comp/cs4218/Utility.java
+++ src/sg/edu/nus/comp/cs4218/Utility.java
@@ -2,6 +2,7 @@ package sg.edu.nus.comp.cs4218;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -13,6 +14,23 @@ import sg.edu.nus.comp.cs4218.Constants;
  * Created by thientran on 2/7/17.
  */
 public class Utility {
+	public static final int MONTHS_PER_YEAR = 12;
+    public static final int CALENDAR_ROW_SIZE = 6;
+    public static final int CALENDAR_COL_SIZE = 7;
+    public static final int YEAR_ROW_SIZE = 4;
+    public static final int YEAR_COL_SIZE = 3;
+    public static final String NEW_LINE = "\n";
+    public static final String SPACE_SEPARATOR = "\\s+";
+    public static final String SPACE_BETWEEN_MONTH = "        ";
+	public static final String WHITE_SPACE = " ";
+	public static final String ONE_SPACE = " ";
+	public static final String TWO_SPACE = "  ";
+	public static final String THREE_SPACE = "   ";
+    public static final String[] MONTH_TO_TEXT = {
+        	"Jannuary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    };
+    
+	
 	/**
 	 * Return 2D array of default value
 	 * @param rowSize
@@ -139,7 +157,7 @@ public class Utility {
 	 * @return 2D calendar array
 	 */
 	public static int[][] getCalendarArrayForMonth(int month, int year, boolean mondayFirst) {
-		int[][] calendarArr = initArray(Constants.Common.CALENDAR_ROW_SIZE, Constants.Common.CALENDAR_COL_SIZE, -1);
+		int[][] calendarArr = initArray(CALENDAR_ROW_SIZE, CALENDAR_COL_SIZE, -1);
 		int maxDay = getNumberOfDayForMonth(month, year);
 		int dayIndex = getDayOfWeek(1, month, year);
 		if (mondayFirst) {
@@ -147,8 +165,8 @@ public class Utility {
 		}
 		
 		for (int i = 1; i <= maxDay; i++) {
-			int rowIndex = dayIndex / Constants.Common.CALENDAR_COL_SIZE;
-			int colIndex = dayIndex % Constants.Common.CALENDAR_COL_SIZE;
+			int rowIndex = dayIndex / CALENDAR_COL_SIZE;
+			int colIndex = dayIndex % CALENDAR_COL_SIZE;
 			calendarArr[rowIndex][colIndex] = i;
 			dayIndex++;
 		}
@@ -157,42 +175,134 @@ public class Utility {
 	}
 	
 	/**
-	 * Print calendar title
+	 * Create calendar title string
 	 * @param month
 	 * @param year
 	 */
-	public static void printCalTitle(int month, int year) {
+	public static String generateCalTitle(int month, int year) {
 		int calendarWidth = 20;
-		String calendarTitle = Constants.Common.MONTH_TO_TEXT[month - 1] + ", " + year;
+		StringBuilder output = new StringBuilder();
+		
+		String calendarTitle = MONTH_TO_TEXT[month - 1] + ", " + year;
 		int space = (calendarWidth - calendarTitle.length()) / 2;
 		for (int i = 0; i < space; i++) {
-			System.out.print(" ");
+			output.append(ONE_SPACE);
 		}
 		
-		System.out.print(calendarTitle);
+		output.append(calendarTitle);
+		
 		for (int i = 0; i < space; i++) {
-			System.out.print(" ");
-		}		
+			output.append(ONE_SPACE);
+		}
+		
+		return output.toString();
 	}
 	
 	/**
-	 * Print calendar headers
+	 * Generate calendar header string
 	 * @param mondayFirst
-	 */
-	public static void printCalHeaders(boolean mondayFirst) {
-		// print headers
+	 */	
+	public static String generateCalHeaders(boolean mondayFirst) {
 		if (mondayFirst) {
-			System.out.print("Mo Tu We Th Fr Sa Su");
+			return "Mo Tu We Th Fr Sa Su";
 		} else {
-			System.out.print("Su Mo Tu We Th Fr Sa");
+			return "Su Mo Tu We Th Fr Sa";
 		}		
 	}
 	
-	/**
-	 * Print space between calendar 2D array
-	 */
-	public static void printMonthSpace() {
-		System.out.print(Constants.Common.SPACE_BETWEEN_MONTH);
+	public static String generateCalendarForMonthYear(int month, int year, boolean mondayFirst) {
+		int[][] monthArr = Utility.getCalendarArrayForMonth(month, year, mondayFirst);
+		StringBuilder output = new StringBuilder();
+		output.append(Utility.generateCalTitle(month, year));
+		output.append(NEW_LINE);
+		
+		output.append(Utility.generateCalHeaders(mondayFirst));
+		output.append(NEW_LINE);
+		
+		for (int i = 0; i < CALENDAR_ROW_SIZE; i++) {
+			for (int j = 0; j < CALENDAR_COL_SIZE; j++) {
+				if (monthArr[i][j] == -1) {
+					output.append(THREE_SPACE);
+				} else if (monthArr[i][j] < 10) {
+					output.append(monthArr[i][j] + TWO_SPACE);
+				} else {
+					output.append(monthArr[i][j] + ONE_SPACE);
+				}
+			}
+			
+			output.append(NEW_LINE);
+		}
+		
+		return output.toString();
+	}
+	
+	public static String generateCalendarForYear(int year, boolean isMondayFirst) {
+		StringBuilder output = new StringBuilder();
+		List<int[][]> yearArray = new ArrayList<int[][]>();
+		for (int month = 1; month <= MONTHS_PER_YEAR; month++) {
+			int[][] monthArr = Utility.getCalendarArrayForMonth(month, year, isMondayFirst);
+			yearArray.add(monthArr);
+		}
+
+		for (int month = 1; month <= MONTHS_PER_YEAR; month += YEAR_COL_SIZE) {
+			output.append(generateMonthCalendarInRow(year, isMondayFirst, month, month + YEAR_COL_SIZE - 1, yearArray));
+			output.append(NEW_LINE);
+		}
+		
+		return output.toString();
+	}
+	
+	public static String generateMonthCalendarInRow(int year, boolean mondayFirst, int startMonth, int endMonth, List<int[][]> yearArray) {
+		StringBuilder output = new StringBuilder();
+		
+		for (int month = startMonth; month <= endMonth; month++) {
+			output.append(Utility.generateCalTitle(month, year));
+			output.append(SPACE_BETWEEN_MONTH);
+			output.append(TWO_SPACE);
+		}
+		output.append(NEW_LINE);
+		
+		for (int month = startMonth; month <= endMonth; month++) {
+			output.append(Utility.generateCalHeaders(mondayFirst));
+			output.append(SPACE_BETWEEN_MONTH);
+			output.append(ONE_SPACE);
+		}
+		output.append(NEW_LINE);
+		
+		for (int i = 0; i < CALENDAR_ROW_SIZE; i++) {
+			for (int month = startMonth; month <= endMonth; month++) {
+				int[][] monthArr = yearArray.get(month - 1);
+				for (int j = 0; j < CALENDAR_COL_SIZE; j++) {
+					if (monthArr[i][j] == -1) {
+						output.append(THREE_SPACE);
+					} else if (monthArr[i][j] < 10) {
+						output.append(monthArr[i][j] + TWO_SPACE);
+					} else {
+						output.append(monthArr[i][j] + ONE_SPACE);
+					}
+				}
+				output.append(SPACE_BETWEEN_MONTH);
+			}
+			output.append(NEW_LINE);
+		}
+		
+		return output.toString();
+	}
+	
+	public static String arrayToString(String[] array) {
+		StringBuilder output = new StringBuilder();
+		for (int i = 0; i < array.length; i++) {
+			output.append(array[i]);
+			if (i != array.length - 1) {
+				output.append(ONE_SPACE);
+			}
+		}
+		
+		return output.toString();
+	}
+	
+	public static String[] stringToArray(String string) {
+		return string.split(SPACE_SEPARATOR);
 	}
 
 	/**
