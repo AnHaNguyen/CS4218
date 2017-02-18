7a9aff2744a5646f86227df81b659d7f90bc9a3f
bihuutue <bihuutue@gmail.com>
2017-02-18 22:01:50 +0800

    Move ef2 dummy files to ef2 folder

--- test-ef2/sg/edu/nus/comp/cs4218/impl/app/DateApplicationTest.java
+++ test-ef2/sg/edu/nus/comp/cs4218/impl/app/DateApplicationTest.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,8 +11,16 @@ import org.junit.Test;
 public class DateApplicationTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPrintCurrentDate() {
+		//[week day] [month] [day] [hh:mm:ss] [time zone][year].
+		DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd hh:mm:ss zzz yyyy");
+				
+		DateApplication dateApp = new DateApplication();
+		String output = dateApp.printCurrentDate("date");
+		Calendar cal = Calendar.getInstance();
+		String expectedOutput = dateFormat.format(cal);
+		
+		assertEquals(expectedOutput, output);
 	}
 
 }
