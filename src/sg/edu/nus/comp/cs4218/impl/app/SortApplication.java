f95cf9160664765f228011adbc0fbd1cc3c9b052
bihuutue <bihuutue@gmail.com>
2017-02-20 02:01:05 +0800

    Case for sort stdin?

--- src/sg/edu/nus/comp/cs4218/impl/app/SortApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/SortApplication.java
@@ -1,12 +1,8 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+
 
 import sg.edu.nus.comp.cs4218.app.Sort;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
@@ -20,10 +16,12 @@ public class SortApplication implements Sort {
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
 		String toSort = "";
 		String result = "";
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(stdin));
 		//Check case invalid args
-		if (args == null) {
-			throw new SortException(Constants.Common.NULL_ARGS);
+		if (args == null || args.length == 0) {
+			//throw new SortException(Constants.Common.NULL_ARGS);
+			toSort = readStdin(br);
+			result = sortAll(toSort);
 		}
 		if (args.length>2) {
 			throw new SortException(Constants.Common.INVALID_NUMBER_ARGUMENTS);
@@ -31,47 +29,61 @@ public class SortApplication implements Sort {
 
 		//Normal sort
 		if (args.length == 1) {
-			toSort = readFile(args[0]);
-			result = sortAll(toSort);
+			//Sort from file
+			if (!args[0].equals("-n")) {
+				toSort = readFile(args[0]);
+				result = sortAll(toSort);	
+			}
+			//Sort from stdin with st words treated as numbers
+			else {
+				ArrayList<String> toSortList = readStdinNum(br);
+				result = sortFirstAsNum(toSort, toSortList);
+			}
+
 		}
-		//Sort with condition of 1st words treated as numbers
+		//Sort from file with condition of 1st words treated as numbers
 		else if (args.length == 2) {
 			if (!args[0].equals("-n")) {
 				throw new SortException(Constants.SortMessage.INVALID_ARGS);
 			}
-			ArrayList<String> toSortList = readNumFile(args[1]);
-			Hashtable<String, String> numTable = new Hashtable<String, String>();
-			for (int i =0; i<toSortList.size(); i++) {
-				String curLine = toSortList.get(i);
-				int index = curLine.indexOf(" ");
-				String firstWord = "";
-				String restLine = "";
-				if (index !=-1) {
-					firstWord = curLine.substring(0, index);
-					restLine = curLine.substring(index);
-				}
-				else {
-					firstWord = curLine;
-				}
-				numTable.put(firstWord, restLine);
-				toSort += firstWord;
-				if (i<toSortList.size()-1) {
-					toSort += System.lineSeparator();
-				}
-			}
-
-			String[] sortedList = sortNumbers(toSort).split(System.lineSeparator());
-			for (int i = 0; i<sortedList.length; i++) {
-				String firstWord = sortedList[i];
-				result = result + firstWord + numTable.get(firstWord);
-				if (i<sortedList.length-1) {
-					result += System.lineSeparator();
-				}
-			}
+			ArrayList<String> toSortList = readFileNum(args[1]);
+			result = sortFirstAsNum(toSort, toSortList);
 		}
 		System.out.println(result);
 	}
 
+	private String sortFirstAsNum(String toSort, ArrayList<String> toSortList) {
+		String result = "";
+		Hashtable<String, String> numTable = new Hashtable<String, String>();
+		for (int i =0; i<toSortList.size(); i++) {
+			String curLine = toSortList.get(i);
+			int index = curLine.indexOf(" ");
+			String firstWord = "";
+			String restLine = "";
+			if (index !=-1) {
+				firstWord = curLine.substring(0, index);
+				restLine = curLine.substring(index);
+			}
+			else {
+				firstWord = curLine;
+			}
+			numTable.put(firstWord, restLine);
+			toSort += firstWord;
+			if (i<toSortList.size()-1) {
+				toSort += System.lineSeparator();
+			}
+		}
+
+		String[] sortedList = sortNumbers(toSort).split(System.lineSeparator());
+		for (int i = 0; i<sortedList.length; i++) {
+			String firstWord = sortedList[i];
+			result = result + firstWord + numTable.get(firstWord);
+			if (i<sortedList.length-1) {
+				result += System.lineSeparator();
+			}
+		}
+		return result;
+	}
 
 	private String readFile(String filePath) {
 		String toSort = "";
@@ -91,10 +103,41 @@ public class SortApplication implements Sort {
 		return toSort;
 	}
 
-	private ArrayList<String> readNumFile(String filePath) {
+	private ArrayList<String> readFileNum(String filePath) {
 		ArrayList<String> toSort = new ArrayList<String>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			String line = br.readLine();
+			while(line!=null){
+				toSort.add(line);
+				line = br.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return toSort;
+	}
+
+	private String readStdin(BufferedReader br) {
+		String toSort = "";
+		try {
+			String line = br.readLine();
+			while (line!=null) {
+				toSort += line;
+				line = br.readLine();
+				if(line!=null) {
+					toSort += System.lineSeparator();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return toSort;
+	}
+
+	private ArrayList<String> readStdinNum(BufferedReader br) {
+		ArrayList<String> toSort = new ArrayList<String>();
+		try {
 			String line = br.readLine();
 			while(line!=null){
 				toSort.add(line);
