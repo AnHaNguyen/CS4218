91ca39230fea25d18fca4b9d402fb08f4a1cb0de
AnHaNguyen <edge.skywalker@gmail.com>
2017-03-05 00:54:41 +0800

    add files for sed and wc

--- src/sg/edu/nus/comp/cs4218/impl/app/SedApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/SedApplication.java
@@ -3,61 +3,49 @@ package sg.edu.nus.comp.cs4218.impl.app;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import sg.edu.nus.comp.cs4218.app.Wc;
+import sg.edu.nus.comp.cs4218.app.Sed;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 
-public class WcApplication implements Wc {
+public class SedApplication implements Sed {
 
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public String printCharacterCountInFile(String args) {
+	public String replaceFirstSubStringInFile(String args) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String printWordCountInFile(String args) {
+	public String replaceAllSubstringsInFile(String args) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String printNewlineCountInFile(String args) {
+	public String replaceFirstSubStringFromStdin(String args) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String printAllCountsInFile(String args) {
+	public String replaceAllSubstringsInStdin(String args) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String printCharacterCountInStdin(String args) {
+	public String replaceSubstringWithInvalidReplacement(String args) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String printWordCountInStdin(String args) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String printNewlineCountInStdin(String args) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String printAllCountsInStdin(String args) {
+	public String replaceSubstringWithInvalidRegex(String args) {
 		// TODO Auto-generated method stub
 		return null;
 	}
