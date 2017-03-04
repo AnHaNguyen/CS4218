3dc301a5cb3fb4f293484097c2328c0bca97d653
AnHaNguyen <edge.skywalker@gmail.com>
2017-03-04 19:59:14 +0800

    fix bugs for glob

--- src/sg/edu/nus/comp/cs4218/impl/cmd/CallCommand.java
+++ src/sg/edu/nus/comp/cs4218/impl/cmd/CallCommand.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -238,6 +239,7 @@ public class CallCommand implements Command {
 				expandedArgs.add(arg);
 			}
 		}
+		Collections.sort(expandedArgs);
 		return expandedArgs;
 	}
 	
