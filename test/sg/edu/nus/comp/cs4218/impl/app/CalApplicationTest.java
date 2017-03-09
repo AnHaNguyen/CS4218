956440fdcb220eaed2611b5eb1490fe43f257753
Thien Tran <thientran1707@gmail.com>
2017-03-10 01:44:40 +0800

    fix calApptest for March

--- test/sg/edu/nus/comp/cs4218/impl/app/CalApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/CalApplicationTest.java
@@ -7,15 +7,15 @@ public class CalApplicationTest {
 	
 	@Test
 	public void testPrintCal() {
-		// current is February 2017
+		// current is March 2017
 		String expectedOuput = 
-				   "   February, 2017   \n" +
+				   "    March, 2017    \n" +
 				   "Su Mo Tu We Th Fr Sa \n" +
 				   "         1  2  3  4  \n" +
 				   "5  6  7  8  9  10 11 \n" +
 				   "12 13 14 15 16 17 18 \n" +
 				   "19 20 21 22 23 24 25 \n" +
-				   "26 27 28             \n" +
+				   "26 27 28 29 30 31    \n" +
 				   "                     \n";
 		CalApplication calApplication = new CalApplication();
 		String output = calApplication.printCal("");
@@ -24,15 +24,15 @@ public class CalApplicationTest {
 	
 	@Test
 	public void testPrintCalMondayFirst() {
-		// current is February 2017
+		// current is March 2017
 		String expectedOuput = 
-				   "   February, 2017   \n" +
+				   "    March, 2017    \n" +
 	   			   "Mo Tu We Th Fr Sa Su \n" +
 	   			   "      1  2  3  4  5  \n" +
 	   			   "6  7  8  9  10 11 12 \n" +
 	   			   "13 14 15 16 17 18 19 \n" +
 	   			   "20 21 22 23 24 25 26 \n" +
-	   			   "27 28                \n" +
+	   			   "27 28 29 30 31       \n" +
 	   			   "                     \n";
 
 		CalApplication calApplication = new CalApplication();
