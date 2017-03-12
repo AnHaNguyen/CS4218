abdc46a9142257c3580bc86a43d373bd66bf29b6
bihuutue <bihuutue@gmail.com>
2017-03-12 22:27:58 +0800

    Minor warning fix

--- src/sg/edu/nus/comp/cs4218/impl/app/SortApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/SortApplication.java
@@ -114,6 +114,7 @@ public class SortApplication implements Sort {
 					toSort += System.lineSeparator();
 				}
 			}
+			br.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new SortException(Constants.Common.INVALID_PATH);
@@ -130,6 +131,7 @@ public class SortApplication implements Sort {
 				toSort.add(line);
 				line = br.readLine();
 			}
+			br.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
