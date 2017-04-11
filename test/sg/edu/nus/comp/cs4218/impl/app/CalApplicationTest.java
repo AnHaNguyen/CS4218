e41356234c428e5ff26af3c25b63b8e956a9ad28
Thien Tran <thientran1707@gmail.com>
2017-04-12 01:19:12 +0800

    fix current cal test

--- test/sg/edu/nus/comp/cs4218/impl/app/CalApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/CalApplicationTest.java
@@ -7,16 +7,16 @@ public class CalApplicationTest {
 	
 	@Test
 	public void testPrintCal() {
-		// current is March 2017
+		// current is April 2017
 		String expectedOuput = 
-				   "     March 2017     \n" +
+				   "     April 2017     \n" +
 				   "Su Mo Tu We Th Fr Sa \n" +
-				   "         1  2  3  4  \n" +
-				   "5  6  7  8  9  10 11 \n" +
-				   "12 13 14 15 16 17 18 \n" +
-				   "19 20 21 22 23 24 25 \n" +
-				   "26 27 28 29 30 31    \n" +
-				   "                     \n";
+				   "                  1  \n" +
+				   "2  3  4  5  6  7  8  \n" +
+				   "9  10 11 12 13 14 15 \n" +
+				   "16 17 18 19 20 21 22 \n" +
+				   "23 24 25 26 27 28 29 \n" +
+				   "30                   \n";
 		CalApplication calApplication = new CalApplication();
 		String output = calApplication.printCal("");
 		assertEquals(expectedOuput, output);
@@ -24,16 +24,16 @@ public class CalApplicationTest {
 	
 	@Test
 	public void testPrintCalMondayFirst() {
-		// current is March 2017
+		// current is April 2017
 		String expectedOuput = 
-				   "     March 2017     \n" +
+				   "     April 2017     \n" +
 	   			   "Mo Tu We Th Fr Sa Su \n" +
-	   			   "      1  2  3  4  5  \n" +
-	   			   "6  7  8  9  10 11 12 \n" +
-	   			   "13 14 15 16 17 18 19 \n" +
-	   			   "20 21 22 23 24 25 26 \n" +
-	   			   "27 28 29 30 31       \n" +
-	   			   "                     \n";
+				   "               1  2  \n" +
+				   "3  4  5  6  7  8  9  \n" +
+				   "10 11 12 13 14 15 16 \n" +
+				   "17 18 19 20 21 22 23 \n" +
+				   "24 25 26 27 28 29 30 \n" +
+				   "                     \n";
 
 		CalApplication calApplication = new CalApplication();
 		String output = calApplication.printCalWithMondayFirst("-m");
